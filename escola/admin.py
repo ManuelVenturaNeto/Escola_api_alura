f411601c972254a825fdcf0006aca2e250645f84
Manuel Ventura <manuueelneto@gmail.com>
2024-10-11 16:30:24 -0300

    Modificações nas permissões e militações de acesso por usuarios

--- escola/admin.py
+++ escola/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin # type: ignore
-from escola.models import Estudante, Curso, Matricula
+from escola.models import Estudante, Curso, Matricula, Campus, Predio, Sala
 
 class Estudantes(admin.ModelAdmin):
     list_display = ('id', 'nome',  'email', 'cpf', 'data_nascimento', 'celular',)
@@ -16,6 +16,27 @@ class Cursos(admin.ModelAdmin):
     search_fields = ('codigo',)
     
 admin.site.register(Curso, Cursos)
+
+class Campi(admin.ModelAdmin):
+    list_display = ('id', 'numero_do_campus', 'limite_de_predios', 'status_campus',)
+    list_display_links = ('id', 'numero_do_campus',)
+    search_fields = ('numero_do_campus',)
+    
+admin.site.register(Campus, Campi)
+
+class Predios(admin.ModelAdmin):
+    list_display = ('id', 'numero_do_predio', 'limite_de_salas', 'status_predio',)
+    list_display_links = ('id', 'numero_do_predio',)
+    search_fields = ('numero_do_predio',)
+    
+admin.site.register(Predio, Predios)
+
+class Salas(admin.ModelAdmin):
+    list_display = ('id', 'numero_da_sala', 'limite_de_alunos', 'status_sala',)
+    list_display_links = ('id', 'numero_da_sala',)
+    search_fields = ('numero_da_sala',)
+    
+admin.site.register(Sala, Salas)
 
 class Matriculas(admin.ModelAdmin):
     list_display = ('id', 'estudante', 'curso', 'periodo',)
--- escola/views.py
+++ escola/views.py
@@ -3,11 +3,14 @@ from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSe
     ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer, \
     CampusSerializer, PredioSerializer, SalaSerializer
 from rest_framework import viewsets, generics, filters # type: ignore
+from django_filters.rest_framework import DjangoFilterBackend # type: ignore
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle # type: ignore
+from escola.throttles import MatriculaAnonRateThrottle
 '''
 from rest_framework.authentication import BasicAuthentication # type: ignore
 '''
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly # type: ignore
-from django_filters.rest_framework import DjangoFilterBackend # type: ignore
+
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
@@ -15,49 +18,58 @@ class EstudanteViewSet(viewsets.ModelViewSet):
     essa funcao não é necessaria aqui pois é chamada lá em settings
     authentication_classes = [BasicAuthentication]
     '''
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Estudante.objects.all()
+    #permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Estudante.objects.all().order_by('id')
     serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     ordering_fields = ['nome']
     search_fields = ['nome', 'cpf']
+    # if IsAdminUser() == True:
+    #     http_method_names = ["get", "post"]
+    http_method_names = ["get", "post"]
     
 class CursoViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Curso.objects.all()
+    #permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Curso.objects.all().order_by('id')
     serializer_class = CursoSerializer
-
+    http_method_names = ["get", "post"]
+    
 class CampusViewSet(viewsets.ModelViewSet):
-    permission_class = [IsAdminUser]
-    queryset = Campus.objects.all()
+    #permission_class = [IsAdminUser]
+    queryset = Campus.objects.all().order_by('id')
     serializer_class = CampusSerializer
-
+    http_method_names = ["get", "post"]
+    
 class PredioViewSet(viewsets.ModelViewSet):
-    permission_class = [IsAdminUser]
-    queryset = Predio.objects.all()
+    #permission_class = [IsAdminUser]
+    queryset = Predio.objects.all().order_by('id')
     serializer_class = PredioSerializer
+    http_method_names = ["get", "post"]
 
 class SalaViewSet(viewsets.ModelViewSet):
-    permission_class = [IsAdminUser]
-    queryset = Sala.objects.all()
+    #permission_class = [IsAdminUser]
+    queryset = Sala.objects.all().order_by('id')
     serializer_class = SalaSerializer
+    http_method_names = ["get", "post"]
     
 class MatriculaViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    queryset = Matricula.objects.all()
+    #permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Matricula.objects.all().order_by('id')
     serializer_class = MatriculaSerializers
+    throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle]
+    http_method_names = ["get", "post"]
 
 class ListaMatriculaEstudante(generics.ListAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [IsAuthenticatedOrReadOnly]
     def get_queryset(self):
-        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by('id')
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
     
 class ListaMatriculaCurso(generics.ListAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    #permission_classes = [IsAuthenticatedOrReadOnly]
     def get_queryset(self):
-        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by('id')
         return queryset
     serializer_class = ListaMatriculasCursoSerializer
     
